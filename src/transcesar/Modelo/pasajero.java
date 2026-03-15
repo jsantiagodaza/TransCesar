913a002ebe0437c72f9b7773cc7b34ec162a9686
Juan Esteban Martinez Guerra <juanestebanmartinez@unicesar.edu.co>
2026-03-15 00:06:25 -0500

    feat: Agregación de atributos e import para fecha de nacimiento (se utilizara a futuro) a la clase pasajero

--- src/transcesar/Modelo/pasajero.java
+++ src/transcesar/Modelo/pasajero.java
@@ -1,10 +1,18 @@
 
 package transcesar.Modelo;
 
+import java.time.LocalDate;
 
-
-public class pasajero {
+public class pasajero{
     
+    private String id;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private int edad;
+    private String direccion;   
+    
+      
     
     
     
