5274af5ae2e83ee15af31c1185a50906ddc7ade9
Juan Esteban Martinez Guerra <juanestebanmartinez@unicesar.edu.co>
2026-03-15 00:21:20 -0500

    feat: Se creo la clase pasajeroestudiante y le heredo los atributos de la clase pasajero

---
+++ src/transcesar/Modelo/pasajeroestudiante.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package transcesar.Modelo;
+
+/**
+ *
+ * @author PC
+ */
+public class pasajeroestudiante extends pasajero {
+    
+    public pasajeroestudiante(String id, String nombre, String apellido, String telefono, int edad, String direccion, String idticket) {
+        super(id, nombre, apellido, telefono, edad, direccion, idticket);
+    }
+    
+}
