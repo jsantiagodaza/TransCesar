a39ae058ae44a3f88dc84d3bb82223bb0624b945
jainer enrique cuello castilla <jainerecuello@unicesar.edo.co>
2026-03-14 23:00:17 -0500

    creacion de constructor

--- src/transcesar/Modelo/vehiculo.java
+++ src/transcesar/Modelo/vehiculo.java
@@ -9,6 +9,25 @@ public abstract class Vehiculo {
     protected boolean disponible;
     protected double tarifaBase;
     
-    
+    public Vehiculo(String placa, String ruta) {
+        this.placa = placa;
+        this.ruta = ruta;
+        this.pasajerosActuales = 0;
+        this.disponible = true;
+    }
+
+    public boolean tieneCupos() {
+        return pasajerosActuales < capacidadMaxima;
+    }
+
+    public int getCuposDisponibles() {
+        return capacidadMaxima - pasajerosActuales;
+    }
+
+    public void agregarPasajero() {
+        if (tieneCupos()) pasajerosActuales++;
+    }
+
+  
 }
 
