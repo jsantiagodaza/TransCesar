8e789abaa2e440fa6e45e26316d1f6df789c1575
Juan Esteban Martinez Guerra <juanestebanmartinez@unicesar.edu.co>
2026-03-15 01:18:26 -0500

    feat: Se creo el metodo CREATE en la clase ticketDAO

--- src/transcesar/persistencia/ticketDAO.java
+++ src/transcesar/persistencia/ticketDAO.java
@@ -6,6 +6,13 @@ import java.util.ArrayList;
 public class ticketDAO {
     
      private ArrayList<ticket> tickets = new ArrayList<>();
+     
+     
+        // CREATE
+         public void guardar(ticket t) {
+        tickets.add(t);
+        System.out.println("Ticket guardado: " + t.getCodigoTicket());
+    }
     
     
     
