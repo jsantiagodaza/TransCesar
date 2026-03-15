6ea1ee5d02463fc9a9324026865293193218393d
Juan Esteban Martinez Guerra <juanestebanmartinez@unicesar.edu.co>
2026-03-15 02:44:38 -0500

    feat: Se creo la clase ticketServices y se agrego importes necesarios junto con arraylist

---
+++ src/trancesar/Logica/ticketServices.java
@@ -0,0 +1,20 @@
+package trancesar.Logica;
+
+import Modelo.ticketDAO;
+import transcesar.persistencia.ticketDAO;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.UUID;
+
+
+
+public class ticketServices {
+    
+    private TicketDAO ticketDAO = new TicketDAO();
+    
+    
+    
+    
+    
+}
