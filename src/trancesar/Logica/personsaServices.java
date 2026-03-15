1b2f2413be89d544d14a920465deb17f6445ed27
Juan Esteban Martinez Guerra <juanestebanmartinez@unicesar.edu.co>
2026-03-15 02:04:27 -0500

    feat: Se agrego importes necesarios(a futuro) junto arraylist en la clase personsaService

--- src/trancesar/Logica/personsaServices.java
+++ src/trancesar/Logica/personsaServices.java
@@ -1,8 +1,12 @@
 package trancesar.Logica;
 
-
+import Modelo.persona;
+import transcesar.persistencia.personaDAO;
+import java.util.ArrayList;
 
 public class personsaServices {
+    
+     private PersonaDAO personaDAO = new PersonaDAO();
     
     
     
