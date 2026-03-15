393b37d26281508de35cfc02dc20a211676f8606
Juan Esteban Martinez Guerra <juanestebanmartinez@unicesar.edu.co>
2026-03-15 00:38:29 -0500

    feat: Se creo la clase ticket y se agregaron atributos junto con importes necesarios(a futuro)

--- src/transcesar/Modelo/pasajeroadultomayor.java
+++ src/transcesar/Modelo/pasajeroadultomayor.java
@@ -2,8 +2,10 @@ package transcesar.Modelo;
 
    
 public class pasajeroadultomayor extends pasajero {
+            
     
-    public pasajeroadultomayor(String id, String nombre, String apellido, String telefono, int edad, String direccion, String idticket) {
+   
+      public pasajeroadultomayor(String id, String nombre, String apellido, String telefono, int edad, String direccion, String idticket) {
         super(id, nombre, apellido, telefono, edad, direccion, idticket);
     }
     
