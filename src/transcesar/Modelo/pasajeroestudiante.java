1618b9a218e31627df0bf21924c96ffccc933361
Juan Esteban Martinez Guerra <juanestebanmartinez@unicesar.edu.co>
2026-03-15 00:25:37 -0500

    feat: Se agrego contructor, getter y setter a la clase pasajeroestudiante

--- src/transcesar/Modelo/pasajeroestudiante.java
+++ src/transcesar/Modelo/pasajeroestudiante.java
@@ -12,5 +12,22 @@ public class pasajeroestudiante extends pasajero {
     public pasajeroestudiante(String id, String nombre, String apellido, String telefono, int edad, String direccion, String idticket) {
         super(id, nombre, apellido, telefono, edad, direccion, idticket);
     }
+
+    public pasajeroestudiante(String institucion, String id, String nombre, String apellido, String telefono, int edad, String direccion, String idticket) {
+        super(id, nombre, apellido, telefono, edad, direccion, idticket);
+        this.institucion = institucion;
+    }
+
+    public String getInstitucion() {
+        return institucion;
+    }
+
+    public void setInstitucion(String institucion) {
+        this.institucion = institucion;
+    }
+    
+    
+    
+    
     
 }
