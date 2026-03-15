45309823d885caedd4717d978617f1ede797e66a
Juan Esteban Martinez Guerra <juanestebanmartinez@unicesar.edu.co>
2026-03-15 00:55:30 -0500

    feat: Se agregaron importes necesarios y arraylist a la clase personaDAO

--- src/transcesar/persistencia/personaDAO.java
+++ src/transcesar/persistencia/personaDAO.java
@@ -1,7 +1,14 @@
 
 package transcesar.persistencia;
 
+import Modelo.Persona;
+import java.util.ArrayList;
 
 public class personaDAO {
+    
+       private ArrayList<persona> personas = new ArrayList<>();
+    
+    
+    
     
 }
