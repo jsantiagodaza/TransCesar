7a6205a6390f737ce8cd670ddada5170a87e12c0
Juan Esteban Martinez Guerra <juanestebanmartinez@unicesar.edu.co>
2026-03-15 00:23:32 -0500

    feat: Se creo el atributo institucion para la clase pasajeroestudiante

--- src/transcesar/Modelo/pasajeroestudiante.java
+++ src/transcesar/Modelo/pasajeroestudiante.java
@@ -1,14 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
+
+
 package transcesar.Modelo;
 
-/**
- *
- * @author PC
- */
+
+
 public class pasajeroestudiante extends pasajero {
+    
+        private String institucion;
     
     public pasajeroestudiante(String id, String nombre, String apellido, String telefono, int edad, String direccion, String idticket) {
         super(id, nombre, apellido, telefono, edad, direccion, idticket);
