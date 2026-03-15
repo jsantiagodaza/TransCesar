5268c7d75061738773e52b7c77d27ba55e812dc3
Juan Esteban Martinez Guerra <juanestebanmartinez@unicesar.edu.co>
2026-03-14 23:48:34 -0500

    feat: Se inicializo en el constructor los atributos y se pusieron getter y setter

--- src/transcesar/Modelo/Persona.java
+++ src/transcesar/Modelo/Persona.java
@@ -14,6 +14,67 @@ public class persona {
     private String apellido;
     private String telefono;
     private int edad;
-    private String direccion;       
+    private String direccion;     
+
+    public persona(String id, String nombre, String apellido, String telefono, int edad, String direccion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.edad = edad;
+        this.direccion = direccion;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+    
+    
+    
        
 }
