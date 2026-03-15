f51e61ccab305df529bf7d4e6f5031b23ab55f88
jainer enrique cuello castilla <jainerecuello@unicesar.edo.co>
2026-03-14 23:21:26 -0500

    creacion de clase microbus

---
+++ src/transcesar/Modelo/MicroBus.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package transcesar.Modelo;
+
+/**
+ *
+ * @author 2jcue
+ */
+    public class MicroBus extends Vehiculo {
+
+    public MicroBus(String placa, String ruta) {
+        super(placa, ruta);
+        this.capacidadMaxima = 25;
+        this.tarifaBase = 10000;
+    }
+
+    public String getTipo() { return "MicroBus"; }
+
+    public void imprimirDetalle() {
+        System.out.println("========== MICROBUS ==========");
+        System.out.println("Placa           : " + placa);
+        System.out.println("Ruta            : " + ruta);
+        System.out.println("Capacidad máx.  : " + capacidadMaxima);
+        System.out.println("Pasajeros act.  : " + pasajerosActuales);
+        System.out.println("Cupos disponib. : " + getCuposDisponibles());
+        System.out.println("Tarifa base     : $" + String.format("%,.0f", tarifaBase));
+        System.out.println("Disponible      : " + (disponible ? "Sí" : "No"));
+        System.out.println("==============================");
+    }
+
+    @Override
+    public double getTarifaBase() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
+
