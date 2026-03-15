3bb3ff620b246e5820fba0fe2afafed2aed866b1
Juan Esteban Martinez Guerra <juanestebanmartinez@unicesar.edu.co>
2026-03-15 00:28:40 -0500

    feat: Se creo la clase pasajeroadultomayor y le heredo los atributos de la clase pasajero

---
+++ src/transcesar/Modelo/pasajeroadultomayor.java
@@ -0,0 +1,12 @@
+package transcesar.Modelo;
+
+   
+public class pasajeroadultomayor extends pasajero {
+    
+    public pasajeroadultomayor(String id, String nombre, String apellido, String telefono, int edad, String direccion, String idticket) {
+        super(id, nombre, apellido, telefono, edad, direccion, idticket);
+    }
+    
+    
+    
+}
