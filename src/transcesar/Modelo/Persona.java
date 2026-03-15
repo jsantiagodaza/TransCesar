61c18c86699d8d1a461e836f2404885d1faf06b8
Juan Esteban Martinez Guerra <juanestebanmartinez@unicesar.edu.co>
2026-03-14 23:43:20 -0500

    feat: Creacion de clase Persona y agregación de import para fecha de nacimiento (se utilizara a futuro)

--- src/transcesar/Modelo/Persona.java
+++ src/transcesar/Modelo/Persona.java
@@ -4,7 +4,16 @@
  */
 package transcesar.Modelo;
 
+import java.time.LocalDate;
+
+
 
 public class persona {
-    
+    private String id;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private int edad;
+    private String direccion;       
+       
 }
