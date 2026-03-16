01b5e3147f27b308359ce0fac7ff8c26440b3b3c
jsantiagodaza <jsantiagodaza@unicesar.edu.co>
2026-03-16 15:46:34 -0500

    refactor: se aplicó formato al codigo de main

--- src/transcesar/Main.java
+++ src/transcesar/Main.java
@@ -1,4 +1,3 @@
-
 package transcesar;
 
 import java.util.Scanner;
@@ -8,15 +7,17 @@ import static transcesar.Presentacion.Menu.menuTickets;
 import static transcesar.Presentacion.Menu.menuVehiculos;
 
 public class Main {
-private static Scanner scanner = new Scanner(System.in);
+
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-   
-     int opcionPrincipal;
-          opcionPrincipal = 0;
-        do{
-            
+
+        int opcionPrincipal;
+        opcionPrincipal = 0;
+        do {
+
             opcionPrincipal = Menu.menuPrincipal();
-            switch (opcionPrincipal){
+            switch (opcionPrincipal) {
                 case 1:
                     menuPersonas();
                     break;
@@ -32,26 +33,26 @@ private static Scanner scanner = new Scanner(System.in);
                 default:
                     System.out.println("Esa opción no existe, eliga una válida.5");
             }
-        }while(opcionPrincipal !=4);
+        } while (opcionPrincipal != 4);
         scanner.close(); //cerramos el scanner 
     }
-    
-       public static void menuPersonas() {
+
+    public static void menuPersonas() {
         int opcion;
         do {
-          
+
             System.out.println("\n|          GESTIÓN DE PERSONAS         |");
             System.out.println("| 1 --> Registrar                                          |");
             System.out.println("| 2 --> Listar Todas las personas              |");
             System.out.println("| 3 --> Buscar Persona por cedula            |");
-            System.out.println("| 4 --> Eliminar Nota               |");
-            System.out.println("| 0 --> Volver                      |");
+            System.out.println("| 4 --> Eliminar Nota                                   |");
+            System.out.println("| 0 --> Volver                                               |");
             System.out.print("| Seleccione una opción: ");
             opcion = scanner.nextInt();
             scanner.nextLine(); // Limpiar buffer
 
             switch (opcion) {
-             
+
             }
         } while (opcion != 0);
     }
