d02b42d85ee8dfbb1f02ad1a0d4b49e72fdcb8fe
jsantiagodaza <jsantiagodaza@unicesar.edu.co>
2026-03-16 09:21:39 -0500

    feat: se conectaon las opciones del Menu a Main

--- src/transcesar/Main.java
+++ src/transcesar/Main.java
@@ -3,6 +3,9 @@ package transcesar;
 
 import java.util.Scanner;
 import transcesar.Presentacion.Menu;
+import static transcesar.Presentacion.Menu.menuPersonas;
+import static transcesar.Presentacion.Menu.menuTickets;
+import static transcesar.Presentacion.Menu.menuVehiculos;
 
 public class Main {
 private static Scanner scanner = new Scanner(System.in);
@@ -15,12 +18,16 @@ private static Scanner scanner = new Scanner(System.in);
             opcionPrincipal = Menu.menuPrincipal();
             switch (opcionPrincipal){
                 case 1:
+                    menuPersonas();
                     break;
                 case 2:
+                    menuVehiculos();
                     break;
                 case 3:
+                    menuTickets();
                     break;
                 case 4:
+                    System.out.println("Nos vemos!, cerrando sistema...");
                     break;
             }
         }while(opcionPrincipal !=4);
