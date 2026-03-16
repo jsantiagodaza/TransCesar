911f8f7b667138a4a70087e0729a5b420607a538
jsantiagodaza <jsantiagodaza@unicesar.edu.co>
2026-03-16 09:04:29 -0500

    feat: se agregó el Scanner y su libreria al main, se usará para leer las variables

--- src/transcesar/Main.java
+++ src/transcesar/Main.java
@@ -1,9 +1,14 @@
 
 package transcesar;
-public class Main {
 
+import java.util.Scanner;
+
+public class Main {
+private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
      
+
+        
     }
     
 }
