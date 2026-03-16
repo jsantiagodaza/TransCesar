1c50e5cef25b3adbb28d090767c81cff6816759f
jsantiagodaza <jsantiagodaza@unicesar.edu.co>
2026-03-16 09:05:46 -0500

    se creó la variable opcionPrincipla para el ciclo do-while de los menús

--- src/transcesar/Main.java
+++ src/transcesar/Main.java
@@ -6,8 +6,7 @@ import java.util.Scanner;
 public class Main {
 private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-     
-
+     int opcionPrincipal;
         
     }
     
