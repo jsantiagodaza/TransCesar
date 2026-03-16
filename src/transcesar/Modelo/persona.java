e3b587b5d80a82e7c844b09cc3fcb397b1e9c39e
jsantiagodaza <jsantiagodaza@unicesar.edu.co>
2026-03-15 22:18:37 -0500

    fix: se hizo un comentario basico en la clase Persona.java para que la cmd la reconozca y pueda actualizarse

--- src/transcesar/Modelo/persona.java
+++ src/transcesar/Modelo/persona.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package transcesar.Modelo;
 
-/**
- *
- * @author santi
- */
-public class persona {
-    
+public class Persona {
+    //aquí iran los atributos
 }
