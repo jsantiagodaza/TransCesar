27a7c70deff7e5f3b5de90d7ba494ffcd456aff1
jsantiagodaza <jsantiagodaza@unicesar.edu.co>
2026-03-15 23:23:54 -0500

    feat: se arreglaron inconsitencias de la clase Persona, al momento de hacer merge, no fue reconocida por las incosistencia, agregó manualmente

--- src/transcesar/Modelo/persona.java
+++ src/transcesar/Modelo/persona.java
@@ -1,7 +1,77 @@
 
 package transcesar.Modelo;
-
+import java.time.LocalDate;
 public class Persona {
-    //aquí iran los atributos
+
+public class persona {
+    private String id;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private int edad;
+    private String direccion;     
+
+    public persona(String id, String nombre, String apellido, String telefono, int edad, String direccion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.edad = edad;
+        this.direccion = direccion;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+    
+    
+    
+       
+}
     
 }
