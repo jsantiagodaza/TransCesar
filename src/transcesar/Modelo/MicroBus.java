5ac8d60c26a6f42a5e99335bee13df10c611db5f
jsantiagodaza <jsantiagodaza@unicesar.edu.co>
2026-03-15 23:41:53 -0500

    refactor: se comentó el Override

--- src/transcesar/Modelo/MicroBus.java
+++ src/transcesar/Modelo/MicroBus.java
@@ -30,7 +30,7 @@ package transcesar.Modelo;
         System.out.println("==============================");
     }
 
-    @Override
+  //  @Override
     public double getTarifaBase() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
