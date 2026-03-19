304db5483152098363384c3441ce46c25c4f2074
jainer enrique cuello castilla <jainerecuello@unicesar.edo.co>
2026-03-18 20:42:28 -0500

    feat: cabios en el dao cehiculo

--- src/transcesar/Persistencia/vehiculoDAO.java
+++ src/transcesar/Persistencia/vehiculoDAO.java
@@ -8,6 +8,33 @@ package transcesar.persistencia;
  *
  * @author 2jcue
  */
-public class vehiculoDAO {
-    
-}
+import transcesar.Presentacion.*;
+import transcesar.Modelo.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class VehiculoDAO {
+
+    private static final String ARCHIVO_BUSETA   = "buseta.txt";
+    private static final String ARCHIVO_BUS      = "bus.txt";
+    private static final String ARCHIVO_MICROBUS = "microbus.txt";
+
+    public void guardar(Vehiculo v) {
+        String archivo = getArchivo(v);
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(archivo, true))) {
+            bw.write(v.toCSV());
+            bw.newLine();
+        } catch (IOException e) {
+            System.err.println("Error al guardar vehiculo: " + e.getMessage());
+        }
+    }
+
+    public List<Vehiculo> cargarTodos() {
+        List<Vehiculo> lista = new ArrayList<>();
+        lista.addAll(cargarDesdeArchivo(ARCHIVO_BUSETA,   "Buseta"));
+        lista.addAll(cargarDesdeArchivo(ARCHIVO_BUS,      "Bus"));
+        lista.addAll(cargarDesdeArchivo(ARCHIVO_MICROBUS, "MicroBus"));
+        return lista;
+    }
+
